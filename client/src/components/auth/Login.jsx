abc712d9e67efd10eb9d8346b2ef9e0a0dbbd634
jorge <jorgetarifa33@gmail.com>
2022-06-22 20:49:09 -0500

    Figure out Auth components

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../../styles/auth.css'
+
+const Login = () => {
+  return (
+    <>
+  <div className="background">
+    <div className="shape" />
+    <div className="shape" />
+  </div>
+  <form>
+    <h3>Login Here</h3>
+    <label htmlFor="username">Username</label>
+    <input type="text" placeholder="Email or Phone" id="username" />
+    <label htmlFor="password">Password</label>
+    <input type="password" placeholder="Password" id="password" />
+    <button>Log In</button>
+    <div className="social">
+      <div className="go">
+        <i className="fab fa-google" /> Google
+      </div>
+      <div className="fb">
+        <i className="fab fa-facebook" /> Facebook
+      </div>
+    </div>
+  </form>
+</>
+
+  )
+}
+
+export default Login
